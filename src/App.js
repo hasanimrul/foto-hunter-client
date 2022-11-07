943ffebb851c8d0f0b4d25ec96e8de993bc2cb71
Md. Imrul Hasan <mohammadhasan.imrul@gmail.com>
2022-11-07 23:19:41 +0600

    first commit with setup installation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className='text-5xl bg-orange-400 p-10'>Hello from foto Hunt🇪🇷 </h2>
+    </div>
+  );
+}
+
+export default App;
