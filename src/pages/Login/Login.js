d32c64308a2a26263b87ed540f118024179821e9
Md. Imrul Hasan <mohammadhasan.imrul@gmail.com>
2022-11-08 22:37:13 +0600

    environment local file created

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
                 <div className="text-center lg:text-left">
                     <h1 className="text-5xl font-bold">Login now!</h1>
                 </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 items-end">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 ">
                     <form className="card-body">
                         <div className="form-control">
                             <label className="label">
